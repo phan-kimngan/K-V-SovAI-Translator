f2d8a577e4f32c86479ac59474afeeb9c0fae9c5
Kim Ngan <55738308+phan-kimngan@users.noreply.github.com>
2025-12-01 15:17:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import streamlit as st
 from gtts import gTTS
 import pandas as pd
 from datetime import datetime
-
+import requests
 #from predict import translate_kor_to_vie
 #from predict_2 import translate_vie_to_kor
 def translate_kor_to_vie(text):
     return text
 def translate_vie_to_kor(text):
     return text
-    
-       
+
+API_URL_kor_to_vie = "https://tenacious-von-occludent.ngrok-free.dev/kor2vie" #translate_kor_to_vie
+API_URL_vie_to_kor = "https://tenacious-von-occludent.ngrok-free.dev/vie2kor" #translate_kor_to_vie
 # ==============================
 # 1. PAGE CONFIG
 # ==============================
@@ -367,13 +368,13 @@ if mode == "vi_to_kr":
     right_label = "Korean"
     src_tts_lang = "vi"
     tgt_tts_lang = "ko"
-    translate_func = translate_vie_to_kor
+    translate_func = API_URL_vie_to_kor
 else:
     left_label = "Korean"
     right_label = "Vietnamese"
     src_tts_lang = "ko"
     tgt_tts_lang = "vi"
-    translate_func = translate_kor_to_vie
+    translate_func = API_URL_kor_to_vie
 
 # ==============================
 # 8. LEFT PANEL
@@ -428,7 +429,7 @@ if st.button("🌐 Translate", use_container_width=True):
     text = st.session_state.input_text.strip()
     if text:
         with st.spinner("Translating... ⏳"):
-            result = translate_func(text)
+            result = requests.get(translate_func, params={"text": text})#translate_func(text)            
             st.session_state.translation = result
 
             # SAVE HISTORY
